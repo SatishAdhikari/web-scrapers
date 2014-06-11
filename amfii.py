89264c76afb6658d86c104c2b0d17daced25462a
Vipul <vipul.sharma20@gmail.com>
2014-06-11 13:29:47 +0530

    added code for creating json of extracted data

--- amfii.py
+++ amfii.py
@@ -26,19 +26,31 @@ def main():
     time.sleep(5)
 
     all_tr = driver.find_elements_by_tag_name('tr') # This will take a lot of time !! about 60-90 seconds
-    time.sleep(90)
+    #time.sleep(90)
 
     ans = []
     for tr in all_tr[5:30]:
-        temp = []
+        #temp = []
+        d = dict()
         all_td = tr.find_elements_by_tag_name('td')
         if all_td == []:
             continue
+        if int(all_td[6].text[:2]) != int(time.strftime("%d"))-1:
+            continue
         else:
-            for td in all_td:
-                temp.append(td.text)
-        ans.append(temp)
+            d['Scheme NAV Name'] = all_td[0].text
+            d['ISIN Div Payout/ ISIN Growth'] = all_td[1].text
+            d['ISIN Div Reinvestment'] = all_td[2].text
+            d['Net Asset Value'] = all_td[3].text
+            d['Repurchase Price'] = all_td[4].text
+            d['Sale Price'] = all_td[5].text
+            d['Date'] = all_td[6].text
+        ans.append(d)
     print ans
+
+    f = open('amfii_latest.json', 'w')
+    f.write(ans)
+    f.close()
 
     
     
