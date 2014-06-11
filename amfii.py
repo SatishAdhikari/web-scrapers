11c3cd0f132996797b8bb5788206fe068ec6aa83
Vipul <vipul.sharma20@gmail.com>
2014-06-11 13:46:05 +0530

    json correction

--- amfii.py
+++ amfii.py
@@ -29,7 +29,7 @@ def main():
     #time.sleep(90)
 
     ans = []
-    for tr in all_tr[5:30]:
+    for tr in all_tr[5:1000]:
         #temp = []
         d = dict()
         all_td = tr.find_elements_by_tag_name('td')
@@ -49,7 +49,7 @@ def main():
     print ans
 
     f = open('amfii_latest.json', 'w')
-    f.write(ans)
+    f.write(json.dumps(ans), indent=2)
     f.close()
 
     
